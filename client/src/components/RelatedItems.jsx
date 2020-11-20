3e5ef6ea5471f811943860f3aaa7e0e35bd4c08b
Andrew McRitchie <andrewgmcritchie@gmail.com>
2020-11-20 15:56:53 -0800

    Implemented the relatedItem compoet functionality, as well as added helper funnctions to my main app.  RelatedShoes will display all shoes with matching name to the current shoe on display.

--- client/src/components/RelatedItems.jsx
+++ client/src/components/RelatedItems.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Shoe from './shoe.jsx';
+import axios from 'axios';
 
-const RelatedItems = (props) => (
-  <div class="shoes-row">
-    {props.shoes.map((shoe, index) => {
-      if (index <= 4) {
-        return (
-          <Shoe shoe={shoe}/>
-        );
-      }
-    }
-    )}
-  </div>
-);
+class RelatedItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoes: [],
+      relatedShoes: []
+    };
+    this.getShoes = this.getShoes.bind(this);
+  }
+
+  componentDidMount() {
+    //Update the states of the component
+    this.getShoes((related, shoes) => {
+      this.setState({
+        relatedShoes: related,
+        shoes: shoes
+      });
+    });
+  }
+
+  getShoes(callback) {
+    axios.get('/product')
+      .then((results) => {
+        // Make a copy of the results
+        var sortedShoes = results.data.slice(0);
+        // Sort shoes by views
+        sortedShoes.sort((a, b) => (a.views < b.views) ? 1 : -1);
+        // Filter shoes by name
+        var filteredShoes = sortedShoes.filter((item) => (
+          this.props.currentShoe.shoeName === item.shoeName
+        ));
+        // Send the results to call back
+        callback(filteredShoes, results.data);
+
+      })
+      .catch((err) => {
+        console.error('Error getting shoes', err);
+      });
+  }
+
+  render() {
+    return (
+      <div class="shoes-row">
+        {this.state.relatedShoes.map((shoe, index) => {
+          if (index <= 4) {
+            return (
+              <Shoe shoe={shoe} addShoe={this.props.addShoe} refreshShoes={this.props.refreshShoes}/>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+}
 
 export default RelatedItems;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import Shoe from './shoe.jsx';
 import RelatedItems from './RelatedItems.jsx';
 import TopViewed from './TopViewed.jsx';
 import RecentlyViewed from './RecentlyViewed.jsx';
@@ -9,31 +8,60 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayShoe: '',
-      shoes: []
+      shoes: [],
+      currentShoe: {},
+      recentlyViewed: []
     };
     this.getShoes = this.getShoes.bind(this);
+    this.addToViewed = this.addToViewed.bind(this);
+    this.refreshShoes = this.refreshShoes.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getShoes();
   }
-  getShoes() {
+
+  // Set the recently viewed state
+  addToViewed(shoe) {
+    // Get top five shoes in the recentlyViewed state
+    var displayShoes = this.state.recentlyViewed.slice(0, 5);
+    //Checking if target shoe doesn't in the top 5
+    if (displayShoes.every((item) => (
+      item['SKU'] !== shoe['SKU']
+    ))) {
+      //Add shoe to beginning of the states' array
+      this.setState({
+        recentlyViewed: [shoe].concat(this.state.recentlyViewed)
+      });
+
+    }
+  }
+  // Update the shoes state after a click
+  refreshShoes(shoes) {
+    this.setState({
+      shoes: shoes
+    });
+  }
+
+  // Grab all shoes from the db
+  getShoes(callback) {
     axios.get('/product')
       .then((results) => {
-        console.log('succesful axios get');
+        // Pick a random shoe to be the current shoe placeholder
+        var randomShoe = results.data[Math.floor(Math.random() * results.data.length)];
+
         this.setState({
-          shoes: results.data
+          shoes: results.data,
+          currentShoe: randomShoe,
+          recentlyViewed: [randomShoe].concat(this.state.recentlyViewed)
         });
-        console.log('shoes', this.state.shoes);
+        console.log('The current shoe selected is: ', randomShoe);
       })
       .catch((err) => {
         console.error('error', err);
       });
   }
-  getRelatedShoes() {
 
-  }
   render() {
     return (
       <div id="main-component">
@@ -41,19 +69,19 @@ class App extends React.Component {
           <div class="carousel-heading">
             <h2>Related Items</h2>
           </div>
-          <RelatedItems shoes={this.state.shoes} />
+          <RelatedItems getShoes={this.getShoes} shoes={this.state.shoes} currentShoe={this.state.currentShoe} addShoe={this.addToViewed} refreshShoes={this.refreshShoes}/>
         </div>
         <div class="carousel">
           <div class="carousel-heading">
             <h2>Top Viewed</h2>
           </div>
-          <TopViewed shoes={this.state.shoes} />
+          <TopViewed shoes={this.state.shoes} currentShoe={this.state.currentShoe} addShoe={this.addToViewed} refreshShoes={this.refreshShoes}/>
         </div>
         <div class="carousel">
           <div class="carousel-heading">
             <h2>Recently Viewed</h2>
           </div>
-          <RecentlyViewed shoes={this.state.shoes} />
+          <RecentlyViewed shoes={this.state.shoes} currentShoe={this.state.currentShoe} addShoe={this.addToViewed} recent={this.state.recentlyViewed} refreshShoes={this.refreshShoes}/>
         </div>
       </div>
     );
